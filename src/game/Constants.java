22a5e4a25af875509420d1a47ef2a7da2aac4388
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2019-01-02 16:46:13 +0530

    [Adharsh] Adds constants for attacks and ability types

---
+++ src/game/Constants.java
@@ -0,0 +1,10 @@
+package game;
+
+public interface Constants {
+    String FIRE = "FIRE";
+    String WATER = "WATER";
+    String ELECTRIC = "ELECTRIC";
+    String DASH = "Dash";
+    String THUNDER_SHOCK = "ThunderShock";
+    String BLAZE = "Blaze";
+}
